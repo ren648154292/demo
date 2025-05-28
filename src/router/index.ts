ab59b40fc36075f39513d2a0f6e8007e9aff69de
任鹏辉 <648154292@qq.com>
2025-05-28 13:38:52 +0800

    依赖升级

--- src/router/index.ts
+++ src/router/index.ts
@@ -10,7 +10,7 @@ const router = createRouter({
       children: [
         {
           path: '',
-          redirect: '/catgallery'
+          redirect: '/dashboard'
         },
         {
           path: 'catgallery',
